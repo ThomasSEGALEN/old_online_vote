d88604ef54dd85040a404c76a0096535204696ce
ThomasSEGALEN <t.segalen@dcsin.lan>
2022-12-19 12:15:23 +0100

    clear routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\GroupController;
-use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserController;
@@ -25,28 +24,46 @@ Route::group(['middleware' => 'auth'], function () {
         return view('dashboard');
     })->name('dashboard');
 
-    Route::resource('users', UserController::class);
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
-    Route::resource('roles', RoleController::class);
+    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RoleController::class, 'create'])->name('roles.create');
+    Route::post('/roles', [RoleController::class, 'store'])->name('roles.store');
+    Route::get('/roles/{role}', [RoleController::class, 'show'])->name('roles.show');
+    Route::get('/roles/{role}/edit', [RoleController::class, 'edit'])->name('roles.edit');
+    Route::put('/roles/{role}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->name('roles.destroy');
 
-    Route::resource('groups', GroupController::class);
+    Route::get('/groups', [GroupController::class, 'index'])->name('groups.index');
+    Route::get('/groups/create', [GroupController::class, 'create'])->name('groups.create');
+    Route::post('/groups', [GroupController::class, 'store'])->name('groups.store');
+    Route::get('/groups/{group}', [GroupController::class, 'show'])->name('groups.show');
+    Route::get('/groups/{group}/edit', [GroupController::class, 'edit'])->name('groups.edit');
+    Route::put('/groups/{group}', [GroupController::class, 'update'])->name('groups.update');
+    Route::delete('/groups/{group}', [GroupController::class, 'destroy'])->name('groups.destroy');
 
-    Route::resource('sessions', SessionController::class);
+    Route::get('/sessions', [SessionController::class, 'index'])->name('sessions.index');
+    Route::get('/sessions/create/', [SessionController::class, 'create'])->name('sessions.create');
+    Route::post('/sessions', [SessionController::class, 'store'])->name('sessions.store');
+    Route::get('/sessions/{session}', [SessionController::class, 'show'])->name('sessions.show');
+    Route::get('/sessions/{session}/edit', [SessionController::class, 'edit'])->name('sessions.edit');
+    Route::put('/sessions/{session}', [SessionController::class, 'update'])->name('sessions.update');
+    Route::delete('/sessions/{session}', [SessionController::class, 'destroy'])->name('sessions.destroy');
     
-    Route::resource('sessions.votes', VoteController::class, [
-        'names' => 
-            [
-                'index' => 'votes.index',
-                'create' => 'votes.create',
-                'store' => 'votes.store',
-                'show' => 'votes.show',
-                'edit' => 'votes.edit',
-                'update' => 'votes.update',
-                'destroy' => 'votes.destroy'
-            ]
-    ]);
-
-    Route::get('/sessions/{session}/votes/{vote}/answers/{answer}', [VoteController::class, 'vote'])->middleware('has_voted')->name('votes.vote');
+    Route::get('/votes', [VoteController::class, 'index'])->name('votes.index');
+    Route::get('/votes/create/{sessionId}', [VoteController::class, 'create'])->name('votes.create');
+    Route::post('/votes', [VoteController::class, 'store'])->name('votes.store');
+    Route::get('//votes/{vote}', [VoteController::class, 'show'])->name('votes.show');
+    Route::get('/votes/{vote}/edit', [VoteController::class, 'edit'])->name('votes.edit');
+    Route::put('/votes/{vote}', [VoteController::class, 'update'])->name('votes.update');
+    Route::delete('/votes/{vote}', [VoteController::class, 'destroy'])->name('votes.destroy');
+    Route::get('/votes/{vote}/{answer}', [VoteController::class, 'vote'])->middleware('has_voted')->name('votes.vote');
 });
 
 Route::get('/', function () {
