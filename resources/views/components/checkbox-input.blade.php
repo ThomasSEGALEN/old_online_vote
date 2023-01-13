9aa979bf3f0d9fc721d9a6aa0ee2fd08e55a50a7
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-01-13 12:32:18 +0100

    checked attribute added

--- resources/views/components/checkbox-input.blade.php
+++ resources/views/components/checkbox-input.blade.php
@@ -1,3 +1,3 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'checked' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['type' => 'checkbox', 'class' => 'form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-indigo-500 checked:border-indigo-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {{ $checked ? 'checked' : '' }} {!! $attributes->merge(['type' => 'checkbox', 'class' => 'form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-indigo-500 checked:border-indigo-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer']) !!}>
