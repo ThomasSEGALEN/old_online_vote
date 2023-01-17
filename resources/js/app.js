6dc61ea882830c8c6d9d9c9410fc0a9f4d2ce4e5
ThomasSÉGALEN <t.segalen@dcsin.lan>
2023-01-17 16:38:33 +0100

    import sortablejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import "./bootstrap";
 
 import Alpine from "alpinejs";
 import jQuery from "jquery";
+import SortableJS from "sortablejs";
 
 window.Alpine = Alpine;
 window.$ = jQuery;
+window.Sortable = SortableJS;
 
 Alpine.start();
